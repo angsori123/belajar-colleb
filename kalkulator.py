4e8775c83a8ee71254ea16c3d1e7d6d01b7088fa
angsori123 <156868540+angsori123@users.noreply.github.com>
2024-01-17 11:03:22 +0700

    Create kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,18 @@
+def tambah(a, b):
+  return a + b
+
+print("pilih oprasi : ")
+print("1. penjumlahan : ")
+print("2. pengurangan : ")
+print("3. perkalian : ")
+
+pilih = input("masukan pilihan (1/2/3) : ")
+
+if pilihan in ('1', '2', '3'):
+  angka1 = int(input("masukan bilangan pertama : "))
+  angka2 = int(input("masukan bilangan kedua : "))
+
+if pilihan == '1':
+  print(angka1. "+", angka2, "=", tambah(angka1, angka2))
+else:
+  print("pilihan tidak valid")
